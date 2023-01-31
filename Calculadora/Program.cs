8b7570bb3d52a9df226c3ba672c9415d9038914d
Douglas Leal <douglas.leal@live.com>
2023-01-30 22:03:50 -0300

    Realizando soma de 2 números

--- Calculadora/Program.cs
+++ Calculadora/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Primeiro valor: ");
+float v1 = float.Parse(Console.ReadLine());
+
+Console.Write("Segundo valor: ");
+float v2 = float.Parse(Console.ReadLine());
+
+Console.WriteLine();
+
+Console.WriteLine($"{v1} + {v2} = {v1 + v2}");
