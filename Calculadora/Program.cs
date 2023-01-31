04cacfc6f1ce755dfaaf918fb30a6e175858773b
Douglas Leal <douglas.leal@live.com>
2023-01-30 22:15:15 -0300

    Criado função Menu para apresentar opções para o usuário

--- Calculadora/Program.cs
+++ Calculadora/Program.cs
@@ -1,5 +1,28 @@
-﻿Multiplicar();
+﻿Menu();
 
+
+
+static void Menu()
+{
+    Console.Clear();
+
+    Console.WriteLine("Calculadora");
+    Console.WriteLine("-----------");
+    Console.WriteLine();
+
+    Console.WriteLine("1 - Somar");
+    Console.WriteLine("2 - Subtrair");
+    Console.WriteLine("3 - Multiplicar");
+    Console.WriteLine("4 - Dividir");
+    Console.WriteLine();
+    Console.WriteLine("0 - Sair");
+    Console.WriteLine();
+    Console.WriteLine("-----------");
+    Console.WriteLine();
+
+    Console.Write("Operação desejada: ");
+    short opcao = short.Parse(Console.ReadLine());
+}
 
 static void Somar()
 {
